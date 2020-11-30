0aa408bab4a080f92956bd72568624652a5f6b06
clubedjogos <clube.desenvolvimento.jogos@gmail.com>
2020-11-30 20:17:24 -0300

    First Commit

---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private PlayerController controller;
+
+    private bool jump;
+    private float horizontalMovement;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButton("Jump"))
+        {
+            jump = true;
+        } else
+        {
+            jump = false;
+        }
+
+        controller.Move(horizontalMovement * speed * Time.deltaTime, jump);
+
+    }
+}
---
+++ Assets/Scripts/parallax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+    private float length, startpos;
+    public Transform cam;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (cam.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        
+    }
+}
